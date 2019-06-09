eefb2900cea9999314989eb31afec8f1296f55e7
ls <1721864248@qq.com>
2019-06-09 14:32:11 +0800

    完成第三页

--- public/js/main/ThirdPanel.js
+++ public/js/main/ThirdPanel.js
@@ -4,7 +4,7 @@ class ThirdPanel extends ModuleWidget {
     this.imgEnd = true;
     this.showBordered = true;
     this.showBordered1 = true;
-    this.titleBorderEnd = false;
+    this.flagEnd = true;
   }
   createContent(parent) {
     this._parent = parent;
@@ -15,10 +15,18 @@ class ThirdPanel extends ModuleWidget {
           this.showBorder();
           this.imgEnd = false;
         }
+
+        if(this.heartEnd){
+          img.removeClass('img1');
+
+          // this.text3.css('animation', '1s ease 0s infinite normal both running puffOut')
+          $('<span class="text4 puffOut">生日快乐</span>').appendTo(this.flag)
+
+          this.heartEnd = false;
+        }
       })
   }
   showBorder() {
-    // let text1 = $('<span class="text">HAPPY BIRTHDAY</span>').appendTo(this._img)
     let right = $('<div class="border right"></div>').appendTo(this._img)
       .on('webkitAnimationEnd', e => {
         if (this.showBordered) {
@@ -26,8 +34,20 @@ class ThirdPanel extends ModuleWidget {
           left.css('height', '100%');
           this.showBordered = false;
 
-          text2.css('color', '#000').addClass('text2')
-          // .addClass('animated slideInLeft');
+          text2.css('color', '#000').animate({
+            'left': '-80px'
+          });
+
+          this._img.animate({
+            'opacity': 0
+          }, 'fast', event => {
+            this._img.addClass('img1').animate({
+              'opacity': 1
+            }, 'fast', event1 => {
+              this.showflag();
+              // this.flagEnd = true;
+            });
+          })
         }
       })
     let left = $('<div class="border left"></div>').appendTo(this._img);
@@ -41,8 +61,36 @@ class ThirdPanel extends ModuleWidget {
 
       });
     let bottom = $('<div class="border1 bottom"></div>').appendTo(this._img);
-    let text1 = $('<span class="text">HAPPY BIRTHDAY</span>').appendTo(this._img);
-    let text2 = $('<span class="text">HAPPY BIRTHDAY</span>').appendTo(this._img);
+    // let text1 = $('<span class="text">HAPPY BIRTHDAY</span>').appendTo(this._img);
+    let text2 = this.text2 = $('<span class="text">HAPPY BIRTHDAY</span>').appendTo(this._img);
+  }
+  showflag() {
+    let flag = this.flag = $('<div class="flag animated rollIn"></div>').appendTo(this._img)
+      .on('webkitAnimationEnd', e => {
+        if (this.flagEnd) {
+          this.showText3(flag);
+          this.flagEnd = false;
+          this.text3End = true;
+
+          // this.text2.animate({
+          //   'transform': 'rotate(360deg)'
+          // })
+          // this.text2.addClass('textAnimate');
+          this.text2.remove();
+          $('<span class="text">HAPPY BIRTHDAY</span>').appendTo(this._img).css('color','#000').addClass('textAnimate')
+          // .addClass('animated rotateIn');
+        }
+      });
+
+  }
+  showText3(parent) {
+    let text3 = this.text3 = $('<span class="text3 animated rollIn">生日快乐</span>').appendTo(parent)
+      .on('webkitAnimationEnd', e => {
+        if (this.text3End) {
+          this._img.removeClass('animated slideInRight').addClass('animated bounceIn');
+          this.heartEnd = true;
+        }
+      });
   }
 }
 
